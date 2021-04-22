ecd18a9b40e7ed1ab6a4a9c80d9bc68df45cf2f3
drinkseltzersmokeweed <81999553+drinkseltzersmokeweed@users.noreply.github.com>
2021-04-21 19:08:45 -0500

    Update RemoteWindowsUpdate.ps1

--- RemoteWindowsUpdate.ps1
+++ RemoteWindowsUpdate.ps1
@@ -9,14 +9,17 @@ While(-not $validAnswer)
                 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
                 #Let's install the PSWindowsUpdate app
                 Install-Module PSWindowsUpdate
+                Import-Module PSWindowsUpdate
                 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+                Invoke-WUJob
                 Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
+                Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
         }
         "n" {$validAnswer = $true
                 #Here we need to perform a test for PSWindowsUpdate to be installed locally, then if it's not installed we can exit
                 if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
                     #yay they have it installed already
-                } 
+                }
                 else{
                     Write-Host "PSWindowsUpdate isn't installed, we will quit now."
                     Exit 1
@@ -38,8 +41,10 @@ While(-not $validAnswer)
     {
         "y" {$validAnswer = $true
             #Let's send the commmands needed
+            Invoke-Command -ComputerName $machineName {Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force}
             Invoke-Command -ComputerName $machineName {Set-PSRepository -Name PSGallery -InstallationPolicy Trusted}
             Invoke-Command -ComputerName $machineName {Install-Module PSWindowsUpdate -force}
+            Invoke-Command -ComputerName $machineName {Import-Module PSWindowsUpdate}
             Invoke-Command -ComputerName $machineName {Add-WUServiceManager -MicrosoftUpdate -Confirm:$false}
             Invoke-Command -ComputerName $machineName {Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false}
         }
@@ -79,7 +84,7 @@ While(-not $validAnswer)
         Set-Item WSMan:localhost\client\trustedhosts -value "$machineName" -force
 
         Write-Host "Attempting to install the updates on $machineName"
-        #Invoke-WUJob -ComputerName $machineName -Script {ipmo PSWindowsUpdate; Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot} -RunNow -Confirm:$false | Out-File "\\$machineName\c$\temp\$machineName-$(Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force 
+        #Invoke-WUJob -ComputerName $machineName -Script {ipmo PSWindowsUpdate; Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot} -RunNow -Confirm:$false | Out-File "\\$machineName\c$\temp\$machineName-$(Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
         Invoke-WUJob -ComputerName $machineName -Script {ipmo PSWindowsUpdate; Install-WindowsUpdate -AcceptAll | Out-File C:\temp\PSWindowsUpdate.log } -RunNow -Confirm:$false
 
         Write-Host "Update commmand completed on $machineName"
